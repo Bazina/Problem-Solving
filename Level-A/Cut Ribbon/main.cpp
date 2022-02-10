04494c5f2c01a7b4bfdb971d0c7539a86c328e39
Youssef Bazina <60438793+Bazina@users.noreply.github.com>
2022-02-10 18:06:16 +0200

    committing files

---
+++ Level-A/Cut Ribbon/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+#define vpi vector<pi>
+#define all(v)            ((v).begin()), ((v).end())
+#define maxv(a)      (*max_element((a).begin(), (a).end()))
+#define bsrch(a, n)    (binary_search((a).begin(), (a).end(), n))
+#define srt(a)     (sort((a).begin(), (a).end()))
+#define revsrt(a)     (sort((a).rbegin(), (a).rend()))
+#define rev(a)  (reverse((a).begin(), (a).end()))
+#define sum(a)      (accumulate((a).begin(), (a).end(), 0ll))
+#define cnt(a, i)  (count((a).begin(), (a).end(), i))
+#define coutv(a)     for(int i = 0; i < (a).size(); i++) {cout << (a)[i] << " ";}
+#define cinv(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i];}
+#define cinvP1(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i].first;}
+#define cinvP2(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i].second;}
+#define isfind(a, s)  a.find(s) != (a).npos
+#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define rep(i, a, b)    for(int (i) = a; (i) < (b); (i)++)
+
+
+void solve() {
+    int n, a, b, c, ans = 0;
+    cin >> n >> a >> b >> c;
+
+    for (int i = 0; i <= 4000; ++i) {
+        for (int j = 0; j <= 4000; ++j) {
+            int k = (n - a * i - b * j) / c;
+            if (k < 0) break;
+            int x = a * i, y = b * j, z = c * k;
+            if (n == x + y + z) ans = max(ans, i + j + k);
+        }
+    }
+    cout << ans;
+}
+
+int main() {
+    IOS
+    solve();
+    return 0;
+}
