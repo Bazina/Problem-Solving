3a9bede37b68e5bd985490b44d72a53836774a78
Youssef Bazina <60438793+Bazina@users.noreply.github.com>
2022-02-10 18:07:00 +0200

    committing files

---
+++ Level-C/Paint The Array/main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+#define vpi vector<pi>
+#define all(v)            ((v).begin()), ((v).end())
+#define maxv(a)      (*max_element((a).begin(), (a).end()))
+#define bsrch(a, n)    (binary_search((a).begin(), (a).end(), n))
+#define srt(a)     (sort((a).begin(), (a).end()))
+#define revsrt(a)     (sort((a).rbegin(), (a).rend()))
+#define rev(a)  (reverse((a).begin(), (a).end()))
+#define sum(a)      (accumulate((a).begin(), (a).end(), 0ll))
+#define cnt(a, i)  (count((a).begin(), (a).end(), i))
+#define coutv(a)     for(int i = 0; i < (a).size(); i++) {cout << (a)[i] << " ";}
+#define cinv(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i];}
+#define cinvP1(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i].first;}
+#define cinvP2(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i].second;}
+#define isfind(a, s)  a.find(s) != (a).npos
+#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define rep(i, a, b)    for(int (i) = a; (i) < (b); (i)++)
+
+ll gcd(ll a, ll b) {
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    cinv(a)
+
+    ll gcdEven = a[0], gcdOdd = a[1];
+    bool impossible = false;
+    for (int i = 2; i < n; ++i) {
+        if (i & 1) gcdOdd = gcd(gcdOdd, a[i]);
+        else gcdEven = gcd(gcdEven, a[i]);
+    }
+
+    for (int i = 1; i < n; i += 2) {
+        if (a[i] % gcdEven == 0) {
+            impossible = true;
+            break;
+        }
+    }
+    if (!impossible) {
+        cout << gcdEven << "\n";
+        return;
+    }
+
+    impossible = false;
+    for (int i = 0; i < n; i += 2) {
+        if (a[i] % gcdOdd == 0) {
+            impossible = true;
+            break;
+        }
+    }
+    if (!impossible) {
+        cout << gcdOdd << "\n";
+        return;
+    }
+    cout << 0 << "\n";
+}
+
+int main() {
+    IOS
+    int q;
+    cin >> q;
+    while (q--) solve();
+    return 0;
+}
