93fb5111ff4e624f0a985d277381cb80671dcca8
Youssef Bazina <60438793+Bazina@users.noreply.github.com>
2022-02-12 15:38:02 +0200

    committing files

---
+++ Level-B/Balanced Tunnel/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+#define vpi vector<pi>
+#define all(v)            ((v).begin()), ((v).end())
+#define maxv(a)      (*max_element((a).begin(), (a).end()))
+#define bsrch(a, n)    (binary_search((a).begin(), (a).end(), n))
+#define srt(a)     (sort((a).begin(), (a).end()))
+#define revsrt(a)     (sort((a).rbegin(), (a).rend()))
+#define rev(a)  (reverse((a).begin(), (a).end()))
+#define sum(a)      (accumulate((a).begin(), (a).end(), 0ll))
+#define cnt(a, i)  (count((a).begin(), (a).end(), i))
+#define coutv(a)     for(int i = 0; i < (a).size(); i++) {cout << (a)[i] << " ";}
+#define cinv(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i];}
+#define cinvP1(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i].first;}
+#define cinvP2(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i].second;}
+#define isfind(a, s)  a.find(s) != (a).npos
+#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define rep(i, a, b)    for(int (i) = a; (i) < (b); (i)++)
+
+
+void solve() {
+    int n, j = 0, fine = 0;
+    cin >> n;
+    vector<int> a(n);
+    vector<bool> done(n, false);
+    queue<int> b;
+
+    cinv(a)
+    for (int i = 0; i < n; ++i) {
+        int x;
+        cin >> x;
+        b.push(x);
+    }
+
+    int firstOut = a[0];
+    while (!b.empty()) {
+        done[b.front() - 1] = true;
+        if (firstOut == b.front()) {
+            while (j < n && done[a[j] - 1]) j++;
+            firstOut = a[j];
+        } else fine++;
+        b.pop();
+    }
+
+    cout << fine;
+}
+
+int main() {
+    IOS
+    solve();
+    return 0;
+}
