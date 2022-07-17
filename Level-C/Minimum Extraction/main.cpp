3f5f1998690f7d9eda79cca38c98f2714cde34b0
Youssef Bazina <60438793+Bazina@users.noreply.github.com>
2022-07-18 00:16:59 +0200

    committing files

---
+++ Level-C/Minimum Extraction/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+#define vpi vector<pi>
+#define all(v)            ((v).begin()), ((v).end())
+#define maxv(a)      (*max_element((a).begin(), (a).end()))
+#define bsrch(a, n)    (binary_search((a).begin(), (a).end(), n))
+#define srt(a)     (sort((a).begin(), (a).end()))
+#define revsrt(a)     (sort((a).rbegin(), (a).rend()))
+#define rev(a)  (reverse((a).begin(), (a).end()))
+#define sum(a)      (accumulate((a).begin(), (a).end(), 0ll))
+#define cnt(a, i)  (count((a).begin(), (a).end(), i))
+#define coutv(a)     for(int i = 0; i < (a).size(); i++) {cout << (a)[i] << " ";}
+#define cinv(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i];}
+#define cinvP1(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i].first;}
+#define cinvP2(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i].second;}
+#define isfind(a, s)  a.find(s) != (a).npos
+#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define rep(i, a, b)    for(int (i) = a; (i) < (b); (i)++)
+
+
+void solve() {
+    ll n, maxi = LONG_LONG_MIN;
+    cin >> n;
+
+    priority_queue<ll, vector<ll>, greater<>> a;
+    for (int i = 0; i < n; ++i) {
+        ll x;
+        cin >> x;
+        a.push(x);
+    }
+
+    if (n == 1) cout << a.top() << "\n";
+    else {
+        ll x = -a.top();
+        maxi = a.top();
+        a.pop();
+        while (a.size() > 1) {
+            if (maxi < a.top() + x)
+                maxi = a.top() + x;
+            x = -(a.top() + x) + x;
+            a.pop();
+        }
+        ll dummy = a.top();
+        dummy += x;
+        if (maxi < dummy) maxi = dummy;
+        cout << maxi << "\n";
+    }
+}
+
+int main() {
+    IOS
+    int t;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
