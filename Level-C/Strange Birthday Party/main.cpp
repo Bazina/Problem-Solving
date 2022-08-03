77aef631106801566de601895b85427aff792d57
Youssef Bazina <60438793+Bazina@users.noreply.github.com>
2022-08-03 14:03:45 +0200

    committing files

---
+++ Level-C/Strange Birthday Party/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+#define vpi vector<pi>
+#define all(v)            ((v).begin()), ((v).end())
+#define maxv(a)      (*max_element((a).begin(), (a).end()))
+#define bsrch(a, n)    (binary_search((a).begin(), (a).end(), n))
+#define srt(a)     (sort((a).begin(), (a).end()))
+#define revsrt(a)     (sort((a).rbegin(), (a).rend()))
+#define rev(a)  (reverse((a).begin(), (a).end()))
+#define sum(a)      (accumulate((a).begin(), (a).end(), 0ll))
+#define cnt(a, i)  (count((a).begin(), (a).end(), i))
+#define coutv(a)     for(int i = 0; i < (a).size(); i++) {cout << (a)[i] << " ";}
+#define cinv(a)     for(int i = 1; i <= (a).size(); i++){cin >> (a)[i];}
+#define cinvP1(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i].first;}
+#define cinvP2(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i].second;}
+#define isfind(a, s)  a.find(s) != (a).npos
+#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define rep(i, a, b)    for(int (i) = a; (i) < (b); (i)++)
+
+
+void solve() {
+    ll n, m, j = 1, ans = 0;
+    cin >> n >> m;
+    vector<ll> k(n + 1), c(m + 1);
+    for (int i = 1; i <= n; ++i)
+        cin >> k[i];
+    for (int i = 1; i <= m; ++i)
+        cin >> c[i];
+
+    srt(k);
+
+    for (ll i = n; i >= 1; --i) {
+        if (c[j] < c[k[i]])
+            ans += c[j], j++;
+        else
+            ans += c[k[i]];
+    }
+
+    cout << ans << "\n";
+}
+
+int main() {
+    IOS
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
