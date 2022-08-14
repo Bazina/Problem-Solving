7c372b8007d49401ec2cf139f58d56b159c9c59c
Youssef Bazina <60438793+Bazina@users.noreply.github.com>
2022-08-14 21:36:58 +0200

    committing files

---
+++ SPOJ/Bazinga/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+#define vpi vector<pi>
+#define all(v)            ((v).begin()), ((v).end())
+#define maxv(a)      (*max_element((a).begin(), (a).end()))
+#define bsrch(a, n)    (binary_search((a).begin(), (a).end(), n))
+#define srt(a)     (sort((a).begin(), (a).end()))
+#define revsrt(a)     (sort((a).rbegin(), (a).rend()))
+#define rev(a)  (reverse((a).begin(), (a).end()))
+#define sum(a)      (accumulate((a).begin(), (a).end(), 0ll))
+#define cnt(a, i)  (count((a).begin(), (a).end(), i))
+#define coutv(a)     for(int i = 0; i < (a).size(); i++) {cout << (a)[i] << " ";}
+#define cinv(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i];}
+#define cinvP1(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i].first;}
+#define cinvP2(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i].second;}
+#define isfind(a, s)  a.find(s) != (a).npos
+#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define rep(i, a, b)    for(int (i) = a; (i) < (b); (i)++)
+ll MAXN = 1e7 + 1e6, mod = 1e9 + 7;
+vector<bool> primes(MAXN, true);
+vector<ll> mul(MAXN, 1), PRIMES;
+
+
+void sieve() {
+    // Sieve
+    primes[0] = primes[1] = false;
+    for (ll i = 2; i < MAXN; i += 1 + (i & 1))
+        if (primes[i])
+            for (ll j = i + i; j < MAXN; j += i) {
+                primes[j] = false;
+                mul[j] = (j / i == i) ? 1 : j / i;
+            }
+
+}
+
+void solve() {
+    ll k;
+    cin >> k;
+    cout << PRIMES[k - 1] << '\n';
+}
+
+int main() {
+    IOS
+    sieve();
+    for (int i = 0; i < MAXN; ++i)
+        if (primes[mul[i]])
+            PRIMES.push_back(i);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
