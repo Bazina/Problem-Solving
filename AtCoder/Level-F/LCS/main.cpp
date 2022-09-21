9babfcc1dbb466e68691264b6de212697efb9dd7
Youssef Bazina <60438793+Bazina@users.noreply.github.com>
2022-09-21 12:22:20 +0200

    committing files

---
+++ AtCoder/Level-F/LCS/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+#define vpi vector<pi>
+#define all(v)            ((v).begin()), ((v).end())
+#define maxv(a)      (*max_element((a).begin(), (a).end()))
+#define bsrch(a, n)    (binary_search((a).begin(), (a).end(), n))
+#define srt(a)     (sort((a).begin() + 1, (a).end()))
+#define revsrt(a)     (sort((a).rbegin(), (a).rend()))
+#define rev(a)  (reverse((a).begin(), (a).end()))
+#define sum(a)      (accumulate((a).begin(), (a).end(), 0ll))
+#define cnt(a, i)  (count((a).begin(), (a).end(), i))
+#define coutv(a)     for(int i = 0; i < (a).size(); i++) {cout << (a)[i] << " ";}
+#define cinv(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i];}
+#define cinvP1(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i].first;}
+#define cinvP2(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i].second;}
+#define isfind(a, s)  a.find(s) != (a).npos
+#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define rep(i, a, b)    for(int (i) = a; (i) < (b); (i)++)
+int dp[3002][3002];
+string s1, s2;
+
+int iter() {
+    for (int i = 1; i <= s1.size(); ++i) {
+        for (int j = 1; j <= s2.size(); ++j) {
+            if (s1[i - 1] == s2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[s1.size()][s2.size()];
+}
+
+void solve() {
+    stack<char> s;
+    cin >> s1 >> s2;
+    memset(dp, 0, sizeof dp);
+
+    iter();
+
+    int i = (int) s1.size(), j = (int) s2.size();
+
+    while (i > 0 && j > 0) {
+        if (dp[i - 1][j - 1] == dp[i][j])
+            i--, j--;
+        else if (dp[i - 1][j] == dp[i][j])
+            i--;
+        else if (dp[i][j - 1] == dp[i][j])
+            j--;
+        else {
+            s.push(s1[i - 1]);
+            i--, j--;
+        }
+    }
+
+    while (!s.empty()) {
+        cout << s.top();
+        s.pop();
+    }
+}
+
+int main() {
+    IOS
+    solve();
+    return 0;
+}
