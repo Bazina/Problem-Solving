e2b57dc066d8097f4b30c2e0df79cdf111221172
Youssef Bazina <60438793+Bazina@users.noreply.github.com>
2022-10-13 10:56:32 +0200

    committing files

---
+++ CodeChef/Palindrome Flipping/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+#define vpi vector<pi>
+#define all(v)            ((v).begin()), ((v).end())
+#define maxv(a)      (*max_element((a).begin(), (a).end()))
+#define bsrch(a, n)    (binary_search((a).begin(), (a).end(), n))
+#define srt(a)     (sort((a).begin(), (a).end()))
+#define revsrt(a)     (sort((a).rbegin(), (a).rend()))
+#define rev(a)  (reverse((a).begin(), (a).end()))
+#define sum(a)      (accumulate((a).begin(), (a).end(), 0ll))
+#define cnt(a, i)  (count((a).begin(), (a).end(), i))
+#define coutv(a)     for(int i = 0; i < (a).size(); i++) {cout << (a)[i] << " ";}
+#define cinv(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i];}
+#define cinvP1(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i].first;}
+#define cinvP2(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i].second;}
+#define isfind(a, s)  a.find(s) != (a).npos
+#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define rep(i, a, b)    for(int (i) = a; (i) < (b); (i)++)
+int MAXN = 1e6 + 5, mod = 1e9 + 7;
+
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    int zeros = 0, ones = 0;
+    for (char i: s) {
+        if (i == '0')
+            zeros++;
+        else
+            ones++;
+    }
+
+    if (n & 1) {
+        if (zeros & 1 && (ones & 1) == 0 || ones & 1 && (zeros & 1) == 0)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    } else {
+        if ((zeros & 1) == 0 && (ones & 1) == 0)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+}
+
+int main() {
+    IOS
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
