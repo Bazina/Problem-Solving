b498c821c918673dc34abbd4b6be936e07036b51
Youssef Bazina <60438793+Bazina@users.noreply.github.com>
2023-04-10 02:10:30 +0200

    committing files

---
+++ AtCoder/Level-C/Vacation/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define srt(a)     (sort((a).begin(), (a).end()))
+#define revsrt(a)     (sort((a).rbegin(), (a).rend()))
+#define rev(a)  (reverse((a).begin(), (a).end()))
+#define coutv(a)     for(int i = 0; i < (a).size(); i++) {cout << (a)[i] << " ";}
+#define cinv(a)     for(int i = 0; i < (a).size(); i++){cin >> (a)[i];}
+#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+ll MAXN = 1e6 + 5, mod = 1e9 + 7;
+
+void solve() {
+    ll n;
+    cin >> n;
+    ll a[n];
+    for (int i = 0; i < n; ++i)
+        cin >> a[i];
+}
+
+int main() {
+    IOS
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
