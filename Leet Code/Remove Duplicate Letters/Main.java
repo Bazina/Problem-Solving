870b5227eea150876c9a2db64b1661831bf2afa3
Youssef Bazina <60438793+Bazina@users.noreply.github.com>
2023-12-05 09:55:25 +0200

    committing files

---
+++ Leet Code/Remove Duplicate Letters/Main.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        boolean[] visited = new boolean[26];
+        int[] charLastIndex = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            charLastIndex[s.charAt(i) - 'a'] = i;
+        }
+
+        Stack<Character> monotonicStack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char nextChar = s.charAt(i);
+            if (visited[nextChar - 'a'])
+                continue;
+            while (!monotonicStack.empty() && nextChar < monotonicStack.peek() && i < charLastIndex[monotonicStack.peek() - 'a']) {
+                visited[monotonicStack.pop() - 'a'] = false;
+            }
+
+            visited[nextChar - 'a'] = true;
+            monotonicStack.add(nextChar);
+        }
+
+        char[] answer = new char[monotonicStack.size()];
+        int i = monotonicStack.size() - 1;
+        while (!monotonicStack.empty()) {
+            answer[i--] = monotonicStack.pop();
+        }
+
+        return String.valueOf(answer);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        String words = "xyzaxyz";
+        System.out.println(new Solution().removeDuplicateLetters(words));
+    }
+}
