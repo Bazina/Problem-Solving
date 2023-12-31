01ee891ace0f2fa2902e6981e3d3d82811c2b674
Youssef Bazina <60438793+Bazina@users.noreply.github.com>
2023-12-31 05:42:30 +0200

    Update Main.java

--- Leet Code/Minimum Time to Make Rope Colorful/Main.java
+++ Leet Code/Minimum Time to Make Rope Colorful/Main.java
@@ -1,24 +1,25 @@
 class Solution {
-
-    public int divideConquer(int l, int r, String colors, int[] neededTime) {
-        if (l - r == 0)
-            return 0;
-
-        int mid = (l + r) / 2;
-
-
-    }
-
     public int minCost(String colors, int[] neededTime) {
-        return divideConquer(0, colors.length() - 1, colors, neededTime);
+        char[] colorsChars = colors.toCharArray();
+        int max = neededTime[0], needed = 0;
+
+        for (int i = 1; i < colorsChars.length; i++) {
+            if (colorsChars[i] == colorsChars[i - 1]) {
+                needed += Math.min(neededTime[i], max);
+                max = Math.max(neededTime[i], max);
+            } else {
+                max = neededTime[i];
+            }
+        }
+
+        return needed;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        String colors = "aabaa";
-        int[] neededTime = {1, 2, 3, 4, 1};
-        Solution solution = new Solution();
-        System.out.println(solution.minCost(colors, neededTime));
+        String colors = "abcdee";
+        int[] neededTime = {1, 2, 3, 4, 5, 1};
+        System.out.println(new Solution().minCost(colors, neededTime));
     }
 }
